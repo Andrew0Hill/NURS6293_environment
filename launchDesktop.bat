62287cbd44df7c10b0116db13f115096891e19be
Andrew Hill <andrew.2.hill@cuanschutz.edu>
2024-09-04 15:20:11 -0600

    Cleaning up unused files, fixed launch scripts.

--- launchDesktop.bat
+++ launchDesktop.bat
@@ -1,5 +1,12 @@
+@echo off
 REM Script to launch the desktop from docker-compose.
 
 docker compose up -d
+
+set wait_interval=5
+
+echo "Please wait for %wait_interval% seconds..."
+
+timeout /t %wait_interval% /nobreak
 
 start "" http://localhost:3000
