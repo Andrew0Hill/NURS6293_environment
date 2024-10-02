ec6f711a2583ae7ab0610dc572b2b6eff0e22a72
Andrew Hill <andrew.2.hill@cuanschutz.edu>
2024-10-02 09:24:18 -0600

    Update pgModeler build script.

--- nurs6293_desktop/build_pgmodeler.sh
+++ nurs6293_desktop/build_pgmodeler.sh
@@ -8,14 +8,34 @@
 
 # Compile and install the program to the default prefix (/usr/local)
 INSTALL_PREFIX="/usr/local"
+QT_ROOT="/usr/lib/qt6"
 
 # Configure with Qmake
-/usr/lib/qt6/bin/qmake -r CONFIG +=release \
-                          PREFIX=$INSTALL_PREFIX \
-                          BINDIR=$INSTALL_PREFIX \
-                          PRIVATEBINDIR=$INSTALL_PREFIX \
-                          PRIVATELIBDIR=$INSTALL_PREFIX/lib \
-                          pgmodeler.pro
+"$QT_ROOT/bin/qmake" -r "CONFIG+=release" \
+                        "PREFIX=$INSTALL_PREFIX" \
+                        "BINDIR=$INSTALL_PREFIX" \
+                        "PRIVATEBINDIR=$INSTALL_PREFIX" \
+                        "PRIVATELIBDIR=$INSTALL_PREFIX/lib" \
+                        pgmodeler.pro
 
 # Make and install 
-make -j $(nproc --ignore=1) && make install
+make -j $(nproc) && make install
+
+# Copy dependencies
+mkdir $INSTALL_PREFIX/lib/qtplugins
+mkdir $INSTALL_PREFIX/lib/qtplugins/imageformats
+mkdir $INSTALL_PREFIX/lib/qtplugins/printsupport
+mkdir $INSTALL_PREFIX/lib/qtplugins/platforms
+echo -e "[Paths]\nPrefix=.\nPlugins=lib/qtplugins\nLibraries=lib" > $INSTALL_PREFIX/qt.conf
+cd "$QT_ROOT/lib"
+cp libQt6DBus.so.6 libQt6PrintSupport.so.6 libQt6Widgets.so.6 libQt6Network.so.6 libQt6Gui.so.6 \
+   libQt6Core.so.6 libQt5XcbQpa.so.6 libQt6Svg.so.6 libicui18n.so.* libicuuc.so.* libicudata.so.* $INSTALL_PREFIX/lib
+cd "$QT_ROOT/plugins"
+cp -r imageformats/libqgif.so imageformats/libqico.so imageformats/libqjpeg.so \
+      imageformats/libqsvg.so $INSTALL_PREFIX/lib/qtplugins/imageformats
+cp -r printsupport/libcupsprintersupport.so $INSTALL_PREFIX/lib/qtplugins/printsupport
+cp -r platforms/libqxcb.so platforms/libqoffscreen.so $INSTALL_PREFIX/lib/qtplugins/platforms
+mkdir $INSTALL_PREFIX/lib/qtplugins/tls
+cp -r platforms/tls/* $INSTALL_PREFIX/lib/qtplugins/tls
+
+echo "pgModeler build complete."
