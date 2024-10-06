6f6062ce843b3c6d5bd91fa20b4454a3cbbaffc5
Andrew Hill <andrew.2.hill@cuanschutz.edu>
2024-10-06 16:52:49 -0600

    Added docker compose pull command to launch script

--- launchDesktop.sh
+++ launchDesktop.sh
@@ -2,6 +2,7 @@
 # Script to launch the desktop from docker-compose.
 WAIT_INTERVAL=5
 
+docker compose pull && \
 docker compose up -d && \
 echo "Please wait for $WAIT_INTERVAL seconds..." && \
 sleep $WAIT_INTERVAL && \
