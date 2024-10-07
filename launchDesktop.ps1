1686efb1bca79c99e4dc1e16fe2514d0af1993f1
Andrew0Hill <andrew.2.hill@ucdenver.edu>
2024-10-07 14:15:29 -0600

    Modified launch scripts

--- launchDesktop.ps1
+++ launchDesktop.ps1
@@ -1,16 +1,24 @@
 # Script to launch the desktop from docker-compose.
-$wait_interval = 5
+$wait_interval = 2
 $db_container_name = "nurs6293_database"
 
 docker compose pull; if($?) {docker compose up -d}
 
+Write-Host "Please keep this window open. Once database initialization is complete, a browser window will launch."
+Write-Host "This operation may take a few minutes if you are launching the environment for the first time."
+
+$wait_chars = @("|", "/", "-", "\")
+$index = 0
+
 $health_status = docker inspect --format "{{json .State.Health.Status }}" $db_container_name
 
 while ($health_status -ne """healthy""" ) {
-    Write-Output "Waiting for database to finish initializing..."
+    $index = ($index + 1) % $wait_chars.length
+    $pchar = $wait_chars[$index]
+    Write-Host -NoNewLine "`r$pchar Waiting for database to finish initializing..."
     Start-Sleep -Seconds $wait_interval 
     $health_status = docker inspect --format "{{json .State.Health.Status }}" $db_container_name
 }
 
-Write-Output "Database intiialization complete, launching browser...."
+Write-Host "`nDatabase intiialization complete, launching browser...."
 Start-Process "http://localhost:3000"
--- launchDesktop.sh
+++ launchDesktop.sh
@@ -11,7 +11,7 @@ docker compose pull && docker compose up -d
 echo "Please keep this window open. Once database initialization is complete, a browser window will launch."
 echo "This operation may take a few minutes if you are launching the environment for the first time."
 
-WAIT_CHARS=("|" "/" "—" "\\")
+WAIT_CHARS=("|" "/" "-" "\\")
 N=${#WAIT_CHARS[@]}
 INDEX=0
 
