f495dbe112b8f96b2bb89fccff193d6ed4e19d2c
Andrew Hill <andrew.2.hill@cuanschutz.edu>
2024-10-07 10:35:29 -0600

    Made database wait message cleaner.

--- launchDesktop.sh
+++ launchDesktop.sh
@@ -2,16 +2,25 @@
 # Script to launch the desktop from docker-compose.
 
 # Interval between health checks
-WAIT_INTERVAL=5
+WAIT_INTERVAL=2
 # Name of the database container.
 DB_CONTAINER_NAME="nurs6293_database"
 
 docker compose pull && docker compose up -d 
 
+echo "Please keep this window open. Once database initialization is complete, a browser window will launch."
+echo "This operation may take a few minutes if you are launching the environment for the first time."
+
+WAIT_CHARS=("|" "/" "—" "\\")
+N=${#WAIT_CHARS[@]}
+INDEX=0
+
 while [ $(docker inspect --format "{{json .State.Health.Status }}" $DB_CONTAINER_NAME) != '"healthy"' ]
 do
-    echo "Waiting for database to finish initializing..." && sleep $WAIT_INTERVAL
+    INDEX=$((($INDEX+1) % $N))
+    PCHAR="${WAIT_CHARS[$INDEX]}"
+    printf "\r$PCHAR Waiting for database to finish initializing..." && sleep $WAIT_INTERVAL
 done
 
-echo "Database intiialization complete, launching browser...."
+printf "\nDatabase intiialization complete, launching browser...\n"
 open http://localhost:3000
